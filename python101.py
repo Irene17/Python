14de1483a2271ef0774a7cad1b34deb9b40e2fd3
Irene falco <irenefalco47@gmail.com>
2020-05-09 21:20:56 +0200

    variables

--- python101.py
+++ python101.py
@@ -8,3 +8,16 @@ b = 20
 print ( b + 20 )
 print ( b + 20 - a )
 
+v1 = " first string "
+v2 = " second string "
+print ( v1 , v2 )
+v1 , v2 = v2 , v1
+print ( v1 , v2 )
+
+
+
+
+name = 'SERVICE_CLOUD_ACCOUNTS'
+new_name = name.title().replace('_', ' ')
+print(new_name)
+
