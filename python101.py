c71a67a9dd145c69fd86408547573d7318ef8ab6
Irene falco <irenefalco47@gmail.com>
2020-05-09 20:52:33 +0200

    Variables

--- python101.py
+++ python101.py
@@ -1,2 +1,6 @@
 print('keyword.kwlist')
 print ('False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield')
+
+print("Variables")
+a = 1
+print (a)
